d85f5d409e07bb8433f0a2fc7262b9abd8fba883
y-harshitha-reddy <yeddulaharshitha2005@gmail.com>
2025-03-12 14:28:53 +0530

    Update hedge.py

--- hedge.py
+++ hedge.py
@@ -27,7 +27,7 @@ assets = {
     "MSFT": "Microsoft"
 }
 
-st.title("🚀 Advanced Hedge Fund Simulator 💰")
+st.title("🚀 Advanced Hedge Fund Simulator")
 
 # Fetch real-time data
 def fetch_market_data(tickers):
